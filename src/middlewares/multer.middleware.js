86734c08d2f420adbcd419821b86048908ec536a
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-24 14:24:03 +0530

    Cloudinary and multer setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp'); // Folder where files will be stored
+  },
+  filename: function (req, file, cb) {
+    // Unique filename with original extension
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.originalname)
+  }
+});
+ export const upload = multer({storage,})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+import { log } from "console";
+import fs from "fs"
+
+cloudinary.config({
+
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary=async (localFilePath)=>{
+
+    try{
+        if(!localFilePath) return null
+        //uplaod the file
+     const resposne= await  cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        //file has been uplaoded succesfulll
+        console.log("Uploaded succesfully",resposne.url);
+        return resposne
+        
+
+    }catch(err){
+        fs.unlinkSync(localFilePath)// remove the locaaly saved temp file as the upload got failed
+        return null;
+
+    }
+}
+export {uploadOnCloudinary}
