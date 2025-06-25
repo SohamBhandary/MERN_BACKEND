76e68d7247575b83ae6d960254dae428ebe66f46
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-25 10:14:15 +0530

    Router and controller with debugging

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+import router from "./routes/user.routes.js"
+
+//routes declarion
+app.use("/api/v1/users",router)
+
 
 
 
